fcd7ad71876415f3cf4e15a8c175b7bbeb39a2c0
langahoohlo <langahoohlo@gmail.com>
2024-04-19 15:04:19 +0200

    get environment

--- execute.c
+++ execute.c
@@ -3,24 +3,47 @@
 char *fullpath;
 
 /**
+ * _getenv - get environment variable
+ * @name: name of environment variable
+ * Return: pointer to environment variable
+ */
+
+char *_getenv(const char *name) {
+    extern char **environ;
+    char **env, *separator;
+
+    for (env = environ; *env != NULL; env++) {
+        separator = strchr(*env, '=');
+        if (separator != NULL) {
+            if (strncmp(*env, name, separator - *env) == 0) {
+                return separator + 1;
+            }
+        }
+    }
+    return NULL;
+}
+
+/**
  * pathfinder - finds the correct path for given alias
  * @command: given input to check for path
  * Return: pointer to the string array or NULL if failed
  */
 
-char **pathfinder(char *cmd, char **command)
+char **pathfinder(char *cmd, char **command, char **envp)
 {
     char *current_path, *temp_path;
     char *path_tok;
     size_t arglen = strlen(cmd);
+
     fullpath = NULL;
+    (void) envp;
     if (strchr(cmd, '/') != NULL && access(cmd, F_OK) == 0)
     {
         command[0] = cmd;
         return (command);
     }
     path_tok = NULL;
-    current_path = getenv("PATH");
+    current_path = _getenv("PATH");
     temp_path = strdup(current_path);
     path_tok = strtok(temp_path, ":");
     while (path_tok)
@@ -50,7 +73,7 @@ int execute(char *const command[], char **envp)
 {
 	pid_t id = fork();
 	int status, i;
-	char **temp = pathfinder(command[0], (char **) command);
+	char **temp = pathfinder(command[0], (char **) command, envp);
 
 	if (temp != NULL)
 	{
