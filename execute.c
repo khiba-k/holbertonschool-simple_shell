abd335dfd2df21ea40e4aebe3baafa78590e4941
langahoohlo <langahoohlo@gmail.com>
2024-04-20 09:32:31 +0200

    fix logical error

--- execute.c
+++ execute.c
@@ -44,7 +44,10 @@ char **pathfinder(char *cmd, char **command)
     path_tok = NULL;
     current_path = _getenv("PATH");
     if (current_path == NULL || *current_path == '\0')
+    {
+	    perror("./hsh: 1: ls: not found");
 	    exit(EXIT_FAILURE);
+    }
     temp_path = strdup(current_path);
     path_tok = strtok(temp_path, ":");
     while (path_tok)
